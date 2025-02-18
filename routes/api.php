1ef04a287f707b95e66967d4b5d8dd9c09c3b28e
Deepak Kheterpal <khetarpal.mca@gmail.com>
2025-02-19 00:25:58 +0530

    Adding the routes and making sure all the API(s) are called via /api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
+    //Scoped means these attendee resources are part of an event. and if we will use route binding and call the attendee using /events/attendee, laravel will automatically call the attendee of the given event.
+
+
