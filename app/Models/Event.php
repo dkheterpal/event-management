17590354c23f86c6d03adfade505468202ae6633
Deepak Kheterpal <khetarpal.mca@gmail.com>
2025-02-18 17:47:57 +0530

    Restored models, migrations, and seeders from commit 0ac097f

---
+++ app/Models/Event.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+// use Illuminate\Database\Eloquent\Relations\HasMany;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Event extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        $this->belongsTo( User::class );
+
+    }
+
+    public function attendees(): HasMany
+    {
+        return $this->hasMany( Attendee::class );
+    }
+}
---
+++ database/migrations/2025_02_18_071927_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use \App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+
+
+            //1 to mamy relationship between the user and the events.
+
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
